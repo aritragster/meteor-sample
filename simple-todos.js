cbc33e2888db8e776513310e0046a844558e79e4
aritragster <aritragster@gmail.com>
2015-10-18 08:41:56 +0530

    third commit

--- simple-todos.js
+++ simple-todos.js
@@ -30,12 +30,7 @@ if (Meteor.isClient) {
       var text = event.target.text.value;
  
       // Insert a task into the collection
-      Tasks.insert({
-        text: text,
-        createdAt: new Date(), // current time
-        owner: Meteor.userId(),
-        username: Meteor.user().username
-      });
+     Meteor.call("addTask", text);
  
       // Clear form
       event.target.text.value = "";
@@ -48,12 +43,10 @@ if (Meteor.isClient) {
     Template.task.events({
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {
-        $set: {checked: ! this.checked}
-      });
+      Meteor.call("setChecked", this._id, ! this.checked);
     },
     "click .delete": function () {
-      Tasks.remove(this._id);
+      Meteor.call("deleteTask", this._id);
     }
   });
 
@@ -61,3 +54,27 @@ if (Meteor.isClient) {
       passwordSignupFields: "USERNAME_ONLY"
     });
 }
+
+
+
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+ 
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
